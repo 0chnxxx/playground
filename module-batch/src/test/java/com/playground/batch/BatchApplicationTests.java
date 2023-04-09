4119921082471f11180856dd36163ae618ea7f47
Kim Youngchan <0chn.xxx@gmail.com>
2023-04-10 02:24:50 +0900

    [add] spring project initialize

---
+++ module-batch/src/test/java/com/playground/batch/BatchApplicationTests.java
@@ -0,0 +1,11 @@
+package com.playground.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
